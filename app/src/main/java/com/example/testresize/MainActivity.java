deb2b944350f43b41847e2656905c2d7b50aae3b
Sergey31415 <sssssergo15@gmail.com>
2024-03-04 22:16:28 +0600

    select image from device to resize

--- app/src/main/java/com/example/testresize/MainActivity.java
+++ app/src/main/java/com/example/testresize/MainActivity.java
@@ -1,19 +1,54 @@
 package com.example.testresize;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 import com.github.chrisbanes.photoview.PhotoView;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private PhotoView photoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
-        photoView.setImageResource(R.drawable.image);
+        photoView = findViewById(R.id.photo_view);
+
+        // Открываем файловый менеджер или галерею при нажатии кнопки
+        findViewById(R.id.btnLoadImage).setOnClickListener(view -> openFileChooser());
     }
+
+    private void openFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+            // Теперь у вас есть Uri выбранного изображения, и вы можете что-то с ним сделать.
+            PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
+            photoView.setImageURI(imageUri);
+        }
+    }
+
 }
